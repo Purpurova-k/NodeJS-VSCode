632831d0a0647705700cb18c8d3019cc38d6b458
Purpurova-k <katrin.purpurova@gmail.com>
2022-04-29 19:17:45 +0300

    Extensions for VSC added

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "browser": true,
+    },
+
+    "rules": {
+        "semi": "error",
+        "space-in-parens": "error",
+        "no-var": "error",
+        "prefer-const": "error",
+        "no-unused-vars": "error",
+        "no-multi-spaces": "error",
+        "no-multiple-empty-lines": "error",
+        "no-use-before-define": "error",
+    }
+};
